f21fd0ab315a947e069631137f76c59610514020
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2014-12-09 16:42:31 +0300

    added some example

--- README.md
+++
@@ -1,5 +0,0 @@
-pseudo-cluster
-==============
-
-Scripts for emulating activities in HPC cluster guided by 
-LNL Slurm software.
---
+++ main.py
@@ -0,0 +1,20 @@
+import imp
+import os 
+
+MODULE_EXTENSIONS = ('.py', '.pyc', '.pyo')
+
+def package_contents(package_name):
+    file, pathname, description = imp.find_module(package_name)
+    if file:
+        raise ImportError('Not a package: %r', package_name)
+    # Use a set because some may be both source and compiled.
+    return set([os.path.splitext(module)[0]
+        for module in os.listdir(pathname)
+        if module.endswith(MODULE_EXTENSIONS)])
+
+
+s = package_contents('package')
+for m in s:
+    if m != '__init__':
+        print 'importing ' + m
+        __import__('package.' + m)
